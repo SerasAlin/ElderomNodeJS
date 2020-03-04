73106b829e334577d6226d3e8185a100e6f50d0b
SerasAlin <serasalin96@gmail.com>
2020-03-04 15:20:41 +0200

    Done

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-const async = require("async");
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
